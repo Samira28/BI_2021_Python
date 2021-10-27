16b96fce6613fd3c58afbc337012387d3b1d7255
Samira28 <71223829+Samira28@users.noreply.github.com>
2021-10-27 17:17:19 +0300

    Update units_converter.py

--- units_converter.py
+++ units_converter.py
@@ -2,6 +2,7 @@ def converter_Grey_rad(observ):
     if observ[1] == "Гр":
         rad = int(observ[0])*100
         return rad
-   
+
+
 observ = input().split()
 print(converter_Grey_rad(observ), "рад")
