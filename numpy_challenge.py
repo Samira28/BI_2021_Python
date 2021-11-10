de7f188aaae2524100c60f9940a1ea5fe7675a5c
Samira28 <71223829+Samira28@users.noreply.github.com>
2021-11-10 22:24:18 +0300

    Update numpy_challenge.py

--- numpy_challenge.py
+++ numpy_challenge.py
@@ -5,14 +5,16 @@ array1 = np.arange(1, 4, 1)
 array2 = np.array([1, 2, 3])
 array3 = np.linspace(1, 3, 3)
 
+
 def matrix_multiplication(a, b):
 
-
     result = np.matmul(a, b)
+
+
     return result
 
-def multiplication_check(array):
 
+def multiplication_check(array):
 
     i = 0
     while i < len(array):
@@ -24,7 +26,6 @@ def multiplication_check(array):
 
 
 def multiply_matrices(array):
-
 
     if multiplication_check(array):
             A = array[0]
@@ -40,13 +41,11 @@ def multiply_matrices(array):
 
 def compute_2d_distance(array1, array2):
 
-
     res = np.linalg.norm(array1-array2)
     return res
 
 
 def compute_multidimensional_distance(array1, array2):
-
 
     res = np.linalg.norm(array1-array2)
     return res
@@ -54,10 +53,11 @@ def compute_multidimensional_distance(array1, array2):
 
 def compute_pair_distances(coor):
 
-
     n_coor = len(coor)
     dist = np.zeros((n_coor, n_coor))
     row, col = np.triu_indices(n_coor, 1)
+
+
     dist = np.sqrt(((coor[:, None, :] - coor) ** 2).sum(-1))
     return dist
 
