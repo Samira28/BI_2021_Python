648d80929529ae1c21c67e0874b8b02fa8d85405
Samira28 <71223829+Samira28@users.noreply.github.com>
2021-12-13 22:55:05 +0300

    Update wc.py

--- wc.py
+++ wc.py
@@ -6,39 +6,35 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-#Optional
 parser.add_argument("-w", "--words", action='store_true')
 parser.add_argument("-l", "--lines", action='store_true')
 parser.add_argument("-c", "--length",  action='store_true')
 
 args = parser.parse_args()
 
-
 def w(file):
 
     file = sys.stdin
     words = 0
-    for line in sys.stdin:
+    for line in file:
         words += len(line.split())
     print(words)
-    
-                
+
 def lines_count(file):
 
     file = sys.stdin
     lines = 0
-    for line in sys.stdin:
-            lines += 1
+    for line in file:
+        lines += 1
     print(lines)
-    
+
 def length_count(file):
 
     file = sys.stdin
     length = 0
-    for line in sys.stdin:
-            length += len(list(line))
+    for line in file:
+        length += len(list(line))
     print(length)
-    
 
 if args.words:
     file = sys.stdin
