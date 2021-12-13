98639ef763c2e861169b69554cc55d243b55b844
Samira28 <71223829+Samira28@users.noreply.github.com>
2021-12-13 23:58:10 +0300

    Update uniq.py

--- uniq.py
+++ uniq.py
@@ -2,14 +2,33 @@
 # !source env/bin/activate
 
 import sys
+import argparse
 
-lines = []
-for line in sys.stdin:
-    lines.append(line)
-list_of_unique_lines = []
-unique_lines = set(lines)
+parser.add_argument('infile', metavar = 'FILE', nargs = "?", help = 'input file', default='-')
 
-for line in unique_lines:
-    list_of_unique_lines.append(line)
-for i in reversed(range(len(list_of_unique_lines)-1)):
-    sys.stdout.write("".join(list_of_unique_lines[i]))
+args = parser.parse_args()
+
+file = args.infile
+if file == "-":
+    file = sys.stdin
+    lines = []
+    for line in file:
+        lines.append(line)
+    list_of_unique_lines = []
+    unique_lines = set(lines)
+
+    for line in unique_lines:
+        list_of_unique_lines.append(line)
+    for i in reversed(range(len(list_of_unique_lines)-1)):
+        sys.stdout.write("".join(list_of_unique_lines[i]))
+else:
+    with open(file, "r") as f:
+        for line in f:
+            lines.append(line)
+        list_of_unique_lines = []
+        unique_lines = set(lines)
+
+        for line in unique_lines:
+            list_of_unique_lines.append(line)
+        for i in reversed(range(len(list_of_unique_lines)-1)):
+            sys.stdout.write("".join(list_of_unique_lines[i]))
